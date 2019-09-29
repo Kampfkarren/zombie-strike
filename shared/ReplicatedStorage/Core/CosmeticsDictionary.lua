76a373ca197761ca51e5a53fa28439602994c49f
Kampfkarren <boynedmaster@gmail.com>
2019-09-29 16:30:56 -0700

    Split Cosmetics.Cosmetics

---
+++ shared/ReplicatedStorage/Core/CosmeticsDictionary.lua
@@ -0,0 +1,145 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Items = ReplicatedStorage.Items
+
+return {
+	{
+		Name = "Chill",
+		Type = "Face",
+		Instance = Items.Face_Chill.Face,
+	},
+
+	{
+		Name = "Doge",
+		Type = "LowTier",
+		Instance = Items.Bundle_Doge,
+	},
+
+	{
+		Name = "Ud'zal",
+		Type = "HighTier",
+		Instance = Items.Bundle_Udzal,
+	},
+
+	{
+		Name = "Fire",
+		Type = "Particle",
+		Instance = Items.Particle_Fire.Contents,
+		Image = Items.Particle_Fire.Image,
+	},
+
+	{
+		Name = "Oof",
+		Type = "LowTier",
+		Instance = Items.Bundle_Oof,
+	},
+
+	{
+		Name = "Balls",
+		Type = "Particle",
+		Instance = Items.Particle_Balls.Contents,
+		Image = Items.Particle_Balls.Image,
+	},
+
+	{
+		Name = "Err",
+		Type = "Face",
+		Instance = Items.Face_Err.Face,
+	},
+
+	{
+		Name = "Shiny Teeth",
+		Type = "Face",
+		Instance = Items.Face_ShinyTeeth.Face,
+	},
+
+	{
+		Name = "Super Super Happy Face",
+		Type = "Face",
+		Instance = Items.Face_DevFace.Face,
+	},
+
+	{
+		Name = "Friendly Smile",
+		Type = "Face",
+		Instance = Items.Face_FriendlySmile.Face,
+	},
+
+	{
+		Name = ":3",
+		Type = "Face",
+		Instance = Items.Face_Cat.Face,
+	},
+
+	{
+		Name = "Prankster",
+		Type = "Face",
+		Instance = Items.Face_Prankster.Face,
+	},
+
+	{
+		Name = "Bandage",
+		Type = "Face",
+		Instance = Items.Face_Bandage.Face,
+	},
+
+	{
+		Name = "Skeptic",
+		Type = "Face",
+		Instance = Items.Face_Skeptic.Face,
+	},
+
+	{
+		Name = "Blizzard Beast Mode",
+		Type = "Face",
+		Instance = Items.Face_BlizzardBeastMode.Face,
+	},
+
+	{
+		Name = "Golden Shiny Teeth",
+		Type = "Face",
+		Instance = Items.Face_GoldenShinyTeeth.Face,
+	},
+
+	{
+		Name = "Goofball",
+		Type = "Face",
+		Instance = Items.Face_Goofball.Face,
+	},
+
+	{
+		Name = "Freckled Cheeks",
+		Type = "Face",
+		Instance = Items.Face_FreckledCheeks.Face,
+	},
+
+	{
+		Name = "Zorgo",
+		Type = "Face",
+		Instance = Items.Face_Zorgo.Face,
+	},
+
+	{
+		Name = "Monarch Butterfly Smile",
+		Type = "Face",
+		Instance = Items.Face_Butterfly.Face,
+	},
+
+	{
+		Name = "Yum",
+		Type = "Face",
+		Instance = Items.Face_Yum.Face,
+	},
+
+	{
+		Name = "Light Dominus: the God",
+		Type = "HighTier",
+		Instance = Items.Bundle_God1,
+	},
+
+	{
+		Name = "Thanoid",
+		Type = "HighTier",
+		Instance = Items.Bundle_Thanoid,
+	},
+}
