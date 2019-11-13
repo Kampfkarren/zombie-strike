af2168780de1a7f993148a08f4d0554ce4426f22
Kampfkarren <boynedmaster@gmail.com>
2019-11-13 02:28:17 -0800

    Handle errors in UpdateAsync

--- mission/ServerScriptService/Libraries/DungeonServer.lua
+++ mission/ServerScriptService/Libraries/DungeonServer.lua
@@ -11,30 +11,68 @@ local Dungeon = {}
 local dungeonDataStore = DataStoreService:GetDataStore("DungeonInfo")
 local dungeonTablePromise
 
+local MOCK_FAILURE = false
+
+local totalRetries = 0
+
+local function getPrivateServerId()
+	if RunService:IsStudio() then
+		return "MOCK_PRIVATE_SERVER"
+	else
+		return game.PrivateServerId
+	end
+end
+
+local function updateAsync(key, callback)
+	if RunService:IsStudio() and MOCK_FAILURE then
+		if totalRetries < MOCK_FAILURE then
+			error("Simulated UpdateAsync failure")
+		else
+			callback(MockDungeon)
+			return
+		end
+	end
+
+	dungeonDataStore:UpdateAsync(key, callback)
+end
+
 function Dungeon.GetDungeonTable()
 	if RunService:IsStudio() then
-		return Promise.resolve(MockDungeon)
-	else
-		if not dungeonTablePromise then
-			dungeonTablePromise = Promise.new(function(resolve, reject)
-				coroutine.wrap(function()
+		if not MOCK_FAILURE then
+			return Promise.resolve(MockDungeon)
+		end
+	end
+
+	if not dungeonTablePromise then
+		dungeonTablePromise = Promise.new(function(resolve, reject)
+			coroutine.wrap(function()
+				while true do
 					local success, error = pcall(function()
-						dungeonDataStore:UpdateAsync(game.PrivateServerId, function(value)
+						updateAsync(getPrivateServerId(), function(value)
 							assert(value ~= nil)
 							resolve(value)
+							if totalRetries > 0 then
+								print("finally resolved GetDungeonTable after", totalRetries, "retries")
+							end
 							return value
 						end)
 					end)
 
-					if not success then
-						reject(error)
-					end
-				end)()
-			end)
-		end
+					if success then
+						break
+					else
+						totalRetries = totalRetries + 1
+						print("GetDungeonTable failed:", error)
+						print("retries:", totalRetries)
 
-		return dungeonTablePromise
+						wait(0.5)
+					end
+				end
+			end)()
+		end)
 	end
+
+	return dungeonTablePromise
 end
 
 function Dungeon.GetDungeonData(key)
