867067a5e714f67a3bfb3ce2823f98cf21c08820
Kampfkarren <boynedmaster@gmail.com>
2019-11-15 00:59:59 -0800

    Revert "Separate DressOutfit"
    This reverts commit 306903ee042c4ed98ea0d07a7229b45ba17acd0b.

--- hub/StarterGui/MainGui/App/Store/CosmeticPreview.lua
+++ hub/StarterGui/MainGui/App/Store/CosmeticPreview.lua
@@ -3,12 +3,36 @@ local RunService = game:GetService("RunService")
 local Workspace = game:GetService("Workspace")
 
 local Data = require(ReplicatedStorage.Core.Data)
-local DressOutfit = require(ReplicatedStorage.Libraries.DressOutfit)
 local Roact = require(ReplicatedStorage.Vendor.Roact)
 local Promise = require(ReplicatedStorage.Core.Promise)
 local ViewportFramePreviewComponent = require(ReplicatedStorage.Core.UI.Components.ViewportFramePreviewComponent)
 
 local e = Roact.createElement
+
+-- https://devforum.roblox.com/t/view-port-frame-accessories/227171/2
+local function addAccessory(character, accessory)
+	local attachment = accessory.Handle:FindFirstChildOfClass("Attachment") -- Not all Accessories are guaranteed to have an Attachment - if not, we'll use default hat placement.
+	local weld = Instance.new("Weld")
+	weld.Name = "AccessoryWeld"
+	weld.Part0 = accessory.Handle
+
+	if attachment then
+		-- The found attachment name in the accessory matches an existing attachment in the character rig, which we'll make the weld connect to.
+		local other = character:FindFirstChild(tostring(attachment), true)
+		weld.C0 = attachment.CFrame
+		weld.C1 = other.CFrame
+		weld.Part1 = other.Parent
+	else
+		-- No attachment found. The placement is defined using the legacy hat placement.
+		weld.C1 = CFrame.new(0, character.Head.Size.Y / 2, 0) * accessory.AttachmentPoint:inverse()
+		weld.Part1 = character.Head
+	end
+
+	-- Updates the accessory to be positioned accordingly to the weld we just created.
+	accessory.Handle.CFrame = weld.Part1.CFrame * weld.C1 * weld.C0:inverse()
+	accessory.Parent = character
+	weld.Parent = accessory.Handle
+end
 
 local dummies = {}
 local helmetArmorPreviews = {}
@@ -25,10 +49,41 @@ local function getDummyFor(instance)
 	uuid.Value = instance
 	uuid.Parent = dummy
 
-	dummy.Parent = Workspace
-	DressOutfit(dummy, instance)
-	RunService.Heartbeat:wait()
-	dummy.Parent = nil
+	if instance.ItemType.Value == "BundleSimple" then
+		local contents = instance.Contents.Armor:GetChildren()
+
+		if instance.Contents.Helmet:IsA("Folder") then
+			for _, helmet in pairs(instance.Contents.Helmet:GetChildren()) do
+				table.insert(contents, helmet)
+			end
+		else
+			table.insert(contents, instance.Contents.Helmet)
+		end
+
+		for _, clothing in pairs(contents) do
+			local clothing = clothing:Clone()
+			clothing.Parent = dummy
+
+			if clothing:IsA("Accessory") then
+				addAccessory(dummy, clothing)
+			end
+		end
+	elseif instance.ItemType.Value == "BundleComplex" then
+		dummy.Parent = Workspace
+
+		for _, bodyPart in pairs(instance.Contents.Armor:GetChildren()) do
+			if bodyPart:IsA("BasePart") then
+				dummy.Humanoid:ReplaceBodyPartR15(bodyPart.Name, bodyPart:Clone())
+			elseif bodyPart:IsA("Accessory") then
+				addAccessory(dummy, bodyPart)
+			end
+		end
+
+		dummy.Humanoid:ReplaceBodyPartR15(Enum.BodyPartR15.Head, instance.Contents.Helmet:Clone())
+
+		RunService.Heartbeat:wait()
+		dummy.Parent = nil
+	end
 
 	dummies[instance] = dummy
 
