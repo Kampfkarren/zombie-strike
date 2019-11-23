ced70434fdf6323fc8da23b9053d51e98f02f55e
Kampfkarren <boynedmaster@gmail.com>
2019-11-22 16:47:11 -0800

    Speed boost when opening gates

--- mission/StarterPlayer/StarterPlayerScripts/OpenGate/init.client.lua
+++ mission/StarterPlayer/StarterPlayerScripts/OpenGate/init.client.lua
@@ -1,11 +1,16 @@
 local CollectionService = game:GetService("CollectionService")
+local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Workspace = game:GetService("Workspace")
 
 local Dungeon = require(ReplicatedStorage.Libraries.Dungeon)
 
 local Camera = Workspace.CurrentCamera
+local LocalPlayer = Players.LocalPlayer
 local Shake = ReplicatedStorage.RuddevEvents.Shake
+
+local SPEED_BONUS = 1.25
+local SPEED_TIME = 3.5
 
 local gates = {
 	require(script.City),
@@ -25,6 +30,16 @@ ReplicatedStorage.Remotes.OpenGate.OnClientEvent:connect(function(room)
 	local gate = gate:Clone()
 	CollectionService:AddTag(gate, "LocallyCreated")
 	gate.Parent = Workspace
+
+	local character = LocalPlayer.Character
+	local humanoid = character and character:FindFirstChild("Humanoid")
+	if humanoid then
+		local oldSpeed = humanoid.WalkSpeed
+		humanoid.WalkSpeed = oldSpeed * SPEED_BONUS
+		delay(SPEED_TIME, function()
+			humanoid.WalkSpeed = oldSpeed
+		end)
+	end
 
 	local direction = Camera.CFrame:VectorToObjectSpace(
 		(Camera.CFrame.Position - gateModule.Open(gate).PrimaryPart.Position).Unit
