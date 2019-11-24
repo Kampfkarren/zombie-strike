7b4c05a46af232fb2abbb1973d78c981a454bbf4
Kampfkarren <boynedmaster@gmail.com>
2019-11-23 18:50:46 -0800

    Invincibility frames

--- shared/ServerScriptService/TakeDamage.lua
+++ shared/ServerScriptService/TakeDamage.lua
@@ -1,6 +1,14 @@
+local INVINCIBILITY_TIME = 0.75
 local MOBILE_DAMAGE_BUFF = 0.5
 
+local lastInvincible = {}
+
 return function(player, damage)
+	if tick() - (lastInvincible[player] or 0) <= INVINCIBILITY_TIME then
+		return
+	end
+
+	lastInvincible[player] = tick()
 	local character = player.Character
 
 	if player:FindFirstChild("MobileDamageBuff") then
