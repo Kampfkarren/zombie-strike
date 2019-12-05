c8190cb3b51a66ac89feaf292f8593d767515ff2
Kampfkarren <boynedmaster@gmail.com>
2019-12-04 16:19:34 -0800

    Save after buying brains

--- hub/ServerScriptService/Products/Brains.lua
+++ hub/ServerScriptService/Products/Brains.lua
@@ -3,15 +3,19 @@ local ServerScriptService = game:GetService("ServerScriptService")
 
 local BrainsDictionary = require(ReplicatedStorage.BrainsDictionary)
 local DataStore2 = require(ServerScriptService.Vendor.DataStore2)
+local FastSpawn = require(ReplicatedStorage.Core.FastSpawn)
 
 local MultiplierProduct = {}
 
-DataStore2.Combine("DATA", "Gold")
+DataStore2.Combine("DATA", "Brains")
 
 function MultiplierProduct.Activate(player, receiptInfo)
 	for _, product in pairs(BrainsDictionary) do
 		if product.Product == receiptInfo.ProductId then
 			DataStore2("Brains", player):Increment(product.Brains)
+			FastSpawn(function()
+				DataStore2.SaveAll(player)
+			end)
 
 			return Enum.ProductPurchaseDecision.PurchaseGranted
 		end
