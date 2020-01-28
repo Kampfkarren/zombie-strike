e9421a7f4cbd622ba287efd7585bfd133249e480
Kampfkarren <boynedmaster@gmail.com>
2020-01-27 19:19:53 -0800

    Fix boss lighting

--- src/mission/ServerScriptService/Lighting.server.lua
+++ src/mission/ServerScriptService/Lighting.server.lua
@@ -2,9 +2,9 @@ local Lighting = game:GetService("Lighting")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerStorage = game:GetService("ServerStorage")
 
-local Dungeon = require(ReplicatedStorage.Libraries.Dungeon)
+local GetCodeName = require(ReplicatedStorage.Libraries.GetCodeName)
 
-local lightingMod = ServerStorage.Campaigns[Dungeon.GetDungeonData("Campaign")].Lighting
+local lightingMod = ServerStorage.Campaigns[GetCodeName()].Lighting
 
 for _, property in pairs(lightingMod.Properties:GetChildren()) do
 	Lighting[property.Name] = property.Value
